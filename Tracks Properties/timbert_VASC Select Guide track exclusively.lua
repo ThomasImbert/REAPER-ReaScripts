724c411c9836d948c7e62a1eba3009d69290f34a
Thomas <thomas.imbert.auditeur@lecnam.net>
2022-02-17 00:10:45 +0100

    Update VASC Guide select header (again)

--- Tracks Properties/timbert_VASC Select Guide track exclusively.lua
+++ Tracks Properties/timbert_VASC Select Guide track exclusively.lua
@@ -3,9 +3,9 @@
 -- @version 1.0
 -- @link GitHub repository https://github.com/ThomasImbert/REAPER-ReaScripts
 -- @about
---  Select track by name: exclusively selects 'GUIDE' track.  
+--   Select track by name: exclusively selects 'GUIDE' track.  
 --
---  This script was generated with "Lokasenna_Select tracks by name.lua"  
+--   This script was generated with "Lokasenna_Select tracks by name.lua"  
 -- @changelog 
 --   # Initial Release (2022-02-16)
 
